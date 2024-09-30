cefc792d2cdc01783df1d60c4c52d1b3c8466e64
tranthinhqnam <tranthinhqnam@gmail.com>
2024-09-30 15:33:59 +0700

    Add .gitignore for node_modules

---
+++ cypress/tests/selector.js
@@ -0,0 +1,9 @@
+describe('Element interaction', () => {
+    it('should be able to complete the form', () => {
+        cy.visit("/login");
+        cy.get('#username').type('tomsmith');
+        cy.get('#password').type('SuperSecretPassword!');
+        cy.get('button[type="submit"]').click();
+        cy.wait(3000)
+    });
+});
